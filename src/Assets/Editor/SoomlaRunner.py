82dfcd16abfc78fee38321b8309f37e5785efb6a
refaelos <refaeldakar@gmail.com>
2012-12-30 11:21:02 +0200

    removed test code

--- src/Assets/Editor/SoomlaRunner.py
+++ src/Assets/Editor/SoomlaRunner.py
@@ -14,13 +14,3 @@ project.add_file('System/Library/Frameworks/StoreKit.framework', tree='SDKROOT')
 project.add_other_ldflags('-ObjC')
 
 project.saveFormat3_2()
-
-try:
-    # This will create a new file or **overwrite an existing file**.
-    f = open("/Users/test/test.txt", "w")
-    try:
-        f.write(projectPath + '/Unity-iPhone.xcodeproj/project.pbxproj') # Write a string to a file
-    finally:
-        f.close()
-except IOError:
-    pass
